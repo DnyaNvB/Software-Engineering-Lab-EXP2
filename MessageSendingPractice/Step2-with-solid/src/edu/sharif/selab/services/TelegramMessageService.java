550fc8e2d07053ce771b5443ed68a850ee579711
mobinasalimi <mobina.salimipanah@gmail.com>
2024-11-01 23:13:58 +0330

    modify TelegramMessage Service

--- MessageSendingPractice/Step2-with-solid/src/edu/sharif/selab/services/TelegramMessageService.java
+++ MessageSendingPractice/Step2-with-solid/src/edu/sharif/selab/services/TelegramMessageService.java
@@ -4,17 +4,7 @@ import edu.sharif.selab.models.TelegramMessage;
 public class TelegramMessageService implements TelegramService {
     @Override
     public void sendTelegramMessage(TelegramMessage telegramMessage) {
-        boolean validMessage;
-
-        if (telegramMessage.getSourceId() != null && telegramMessage.getTargetId() != null) {
-            validMessage = validateId(telegramMessage.getSourceId()) && validateId(telegramMessage.getTargetId());
-        } else if (telegramMessage.getSourcePhoneNumber() != null && telegramMessage.getTargetPhoneNumber() != null) {
-            validMessage = validatePhoneNumber(telegramMessage.getSourcePhoneNumber()) && validatePhoneNumber(telegramMessage.getTargetPhoneNumber());
-        } else {
-            validMessage = false;
-        }
-
-        if (validMessage) {
+        if (verifySourceAndTarget(telegramMessage)) {
             if (telegramMessage.getSourceId() != null && telegramMessage.getTargetId() != null) {
                 System.out.println("Sending Telegram message from ID: " + telegramMessage.getSourceId() +
                         " to ID: " + telegramMessage.getTargetId() +
@@ -26,6 +16,16 @@ public class TelegramMessageService implements TelegramService {
             }
         } else {
             throw new IllegalArgumentException("Source or Target information is Not Correct!");
+        }
+    }
+
+    private boolean verifySourceAndTarget(TelegramMessage telegramMessage) {
+        if (telegramMessage.getSourceId() != null && telegramMessage.getTargetId() != null) {
+            return validateId(telegramMessage.getSourceId()) && validateId(telegramMessage.getTargetId());
+        } else if (telegramMessage.getSourcePhoneNumber() != null && telegramMessage.getTargetPhoneNumber() != null) {
+            return validatePhoneNumber(telegramMessage.getSourcePhoneNumber()) && validatePhoneNumber(telegramMessage.getTargetPhoneNumber());
+        } else {
+            return false;
         }
     }
 
